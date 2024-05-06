6e761616592cb1afd9354a6069a756ed6d7c0fb7
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2024-05-06 13:46:35 +0200

    Update Program.cs

--- Oppgaver CSharp/Program.cs
+++ Oppgaver CSharp/Program.cs
@@ -2,90 +2,115 @@
 using System;
 using System.Collections.Concurrent;
 
-class Program
+namespace MyCsharp
 {
-    static void Main()
+
+    class Program
     {
-
-        Program myProgram = new Program();
-
-        myProgram.PrintVars();
-        myProgram.PrintCars();
-        PrintList();
-
-        Person person = new Person("Ola Nordmann", 42);
-
-        Console.WriteLine("Name: " + person.Name);
-        Console.WriteLine("Age: " + person.Age);
-
-    }
-
-    public void PrintVars()
-    {
-        // Definere variabler
-        int age = 42;
-        string Name = "Ola Nordmann";
-        double Temp = 13.37;
-
-        Console.WriteLine("Name: " + Name);
-        Console.WriteLine("Age: " + age);
-        Console.WriteLine("Temp: " + Temp + " Celsius");
-    }
-
-    public void PrintCars()
-    {
-        string[] cars = { "Bmw", "Audi", "Mercedes" };
-
-        for (int i = 0; i < cars.Length; i++)
+        static void Main()
         {
-            Console.WriteLine("Car: " + cars[i]);
-        }
-    }
 
-    public class ShoppingList
-    {
-        private List <string> items;
+            Program myProgram = new Program();
 
-        public ShoppingList()
-        {
-            items = new List<string>();
+            PrintVars();
+            PrintCars();
+            PrintList();
+
+            Person person = new("Ola Nordmann", 42);
+
+            Console.WriteLine("Name: " + person.Name);
+            Console.WriteLine("Age: " + person.Age);
+
+            Numbers();
         }
 
-        public void AddItem(string item)
+        public static void PrintVars()
         {
-            items.Add(item);
+            // Definere variabler
+            int age = 42;
+            string Name = "Ola Nordmann";
+            double Temp = 13.37;
+
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Temp: " + Temp + " Celsius");
         }
 
-        public void PrintItems()
+        public static void PrintCars()
         {
-            foreach (string item in items)
+            string[] cars = { "Bmw", "Audi", "Mercedes" };
+
+            for (int i = 0; i < cars.Length; i++)
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Car: " + cars[i]);
             }
         }
-    }
 
-    static void PrintList()
-    {
-        ShoppingList myShoppingList = new ShoppingList();
-        myShoppingList.AddItem("Milk");
-        myShoppingList.AddItem("Eggs");
-        myShoppingList.AddItem("Bread");
-
-        myShoppingList.PrintItems();
-    }
-
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-
-        public Person(string name, int age)
+        public class ShoppingList
         {
-            Name = name;
-            Age = age;
-        }
-    }
+            private readonly List<string> items;
 
+            public ShoppingList()
+            {
+                items = new List<string>();
+            }
+
+            public void AddItem(string item)
+            {
+                items.Add(item);
+            }
+
+            public void PrintItems()
+            {
+                foreach (string item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        static void PrintList()
+        {
+            ShoppingList myShoppingList = new();
+            myShoppingList.AddItem("Milk");
+            myShoppingList.AddItem("Eggs");
+            myShoppingList.AddItem("Bread");
+
+            myShoppingList.PrintItems();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        static void Numbers()
+        {
+            int x = 8;
+            int y = 5;
+            int sum = x + y;
+
+            Console.WriteLine(sum);
+
+            Decimal a1 = 6.5m;
+            Decimal a2 = 6.3m;
+
+            Decimal value = Decimal.Add(a1, a2);
+
+            Console.WriteLine("Decimal Sum : {0}", 
+                                           value);
+
+        }
+
+
+
+    }
 }
